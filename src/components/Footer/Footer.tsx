8cd21a62c82cf0aaeeb87d207c3db82ce2467ee1
Hvatov Maksim <1032204364@pfur.ru>
2023-04-16 20:17:54 +0300

    Added styles to layout and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import {FooterProps} from "./Footer.props";
+import cn from "classnames"
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
-        <footer {...props}>
-            Footer
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div className={styles.copyright}>OwlTop © 2020 - 2021 Все права защищены</div>
+            <a href={"#"} className={styles.agreement}>Пользовательское соглашение</a>
+            <a href={"#"} className={styles.confidential}>Политика конфиденциальности</a>
         </footer>
     )
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,16 +7,14 @@ import {LayoutProps} from "./Layout.props";
 
 const Layout = ({children}: LayoutProps):JSX.Element => {
     return (
-        <>
-            <Header/>
-            <div>
-                <Sidebar/>
-                <div>
-                    {children}
-                </div>
+        <div className={styles.wrapper}>
+            <Header className={styles.header}/>
+            <Sidebar className={styles.sidebar}/>
+            <div className={styles.body}>
+                {children}
             </div>
-            <Footer/>
-        </>
+            <Footer className={styles.footer}/>
+        </div>
     )
 }
 
